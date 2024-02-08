342bdbbd8e5df84277d61ef9e15c7c8ae1dad536
Winnery05 <elmedhassan2002@gmail.com>
2024-02-08 22:03:49 +0100

    hide adresse bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,19 @@ import { BrowserRouter,Routes,Route } from "react-router-dom"
 import Main from "./components/Main"
 import Settings from "./components/Settings"
 import Footer from "./components/Footer"
+import { useEffect } from "react"
 function App() {
-
+  useEffect(() => {
+    window.addEventListener("load",function() {
+    setTimeout(function(){
+        // This hides the address bar:
+        window.scrollTo(0, 1);
+    }, 0);
+});
+  },[])
   return (
     <>
-        
+
       <BrowserRouter>
         <Routes>
           <Route path="/" >
