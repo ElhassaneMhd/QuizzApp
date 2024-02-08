4d006792af90ea4a0f827ae7f4403baef47fbed8
Winnery05 <elmedhassan2002@gmail.com>
2024-02-08 19:53:59 +0100

    settings icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,16 +6,17 @@ function Header() {
                 <p className=" text-text">Start<span className='text-second'>Quizz</span> </p>
                 <span className="text-textSecond text-lg">let&apos;s <span className='text-second'>play</span> the <span className='text-text'>quiz</span></span>
             </div>
-            <div>
+            <div className="flex items-center">
                 <span className=' border cursor-pointer text-2xl p-1 border-text text-text px-2 rounded-full '>
                      <i className="fa-solid  fa-user hover:scale-105" />
                 </span>
                 
                 <span className='group relative px-2 cursor-pointer text-text '>
-                    <i className="text-2xl fa-solid fa-bars"></i>
+                        <Link  to={'/settings'} className='px-2 text-text flex items-center gap-1 rounded-full m-[1px] hover:bg-light hover:scale-105' ><i className="text-3xl fa-solid fa-gear"></i></Link>
+
+                    {/* <i className="text-2xl fa-solid fa-bars"></i>
                       <div className="absolute shadow-[0_3px_10px_rgb(0,0,0,0.2)] rounded-xl flex flex-col bg-white p-1 right-0 top-5 cursor-pointer translate-x-16 scale-0 group-hover:scale-100 hover:scale-100 hover:translate-x-0 group-hover:translate-x-0 transition hover:duration-300">
-                        <Link  to={'/settings'} className='px-2 text-text flex items-center gap-1 rounded-md m-[1px] hover:bg-light hover:scale-105' >Settings<i className="text-sm fa-solid fa-gear"></i></Link>
-                    </div>
+                    </div> */}
                 </span>
             </div>
         </header>
